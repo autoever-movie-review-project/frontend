291c6e2e19933e77fdbd81469c3627e92f763ae3
friend5hip <jwy97@naver.com>
2024-11-07 22:50:59 +0900

    feat: theme.ts 설정

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,5 +1,59 @@
-const colors = {};
+const colors = {
+  primary: '#FE5489',
+  primaryLight: '#FF8BA8',
+  primaryDark: '#D43966',
+  grayLight: '#84868D',
+  grayDark: '#303133',
+  success: '#10B981',
+  warning: '#FBBF24',
+  error: '#EF4444',
+  neutral50: '#F8FAFC',
+  neutral100: '#F1F5F9',
+  neutral200: '#E2E8F0',
+  neutral300: '#CBD5E1',
+  neutral400: '#94A3B8',
+  neutral500: '#64748B',
+  neutral600: '#475569',
+  neutral700: '#334155',
+  neutral800: '#1E293B',
+  neutral900: '#0F172A',
+  modalBackground: '#00000048',
+  bronze: '#C96344',
+  silver: '#C6C6C6',
+  gold: '#EBC700',
+  diamond: '#06CBBA',
+  master: '#FF033E',
+  kakao: '#FEE500',
+};
+
+const fontSize = {
+  xs: '10px',
+  sm: '13px',
+  md: '16px',
+  lg: '20px',
+  xl: '24px',
+  h4: '20px',
+  h3: '24px',
+  h2: '28px',
+  h1: '36px',
+};
+
+const fontWeight = {
+  light: 200,
+  regular: 400,
+  bold: 700,
+  header: 800,
+};
+
+const borderRadius = {
+  xs: '10px',
+  sm: '15px',
+  md: '30px',
+};
 
 export const theme = {
   colors,
+  fontSize,
+  fontWeight,
+  borderRadius,
 };
