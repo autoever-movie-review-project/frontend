26067b5d25a278c9a348a753f8c0490fa9024620
friend5hip <jwy97@naver.com>
2024-11-16 20:08:45 +0900

    feat: Skeleton 컴포넌트 추가

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 import { theme } from 'styles/theme';
 
 interface ButtonStyleProps {
