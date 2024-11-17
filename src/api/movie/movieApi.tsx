ea44d0a3cee6ae1a7e5028c009337acd49b9bc93
Song JiWoong <016vhsdkdl@gmail.com>
2024-11-17 23:33:34 +0900

    feat: 메인페이지 API 개봉예정영화 연동

--- src/api/movie/movieApi.tsx
+++ src/api/movie/movieApi.tsx
@@ -1,4 +1,47 @@
 import { client, publicClient } from 'api/client';
 
-// publicClient;
-// client;
+//박스오피스 순위리스트 Get (interface:BoxOfficeListResDto)
+export const fetchBoxOfficeMovieList = async () => {
+  try {
+    const response = await client.get('/movie/box-office');
+    return response.data;
+  } catch (error) {
+    console.error('박스오피스 영화 리스트 호출 실패:', error);
+    throw error;
+  }
+};
+
+//인기 영화리스트 Get
+export const fetchPopularMovieList = async () => {
+  try {
+    const response = await client.get('/movie/popular');
+    return response.data;
+  } catch (error) {
+    console.error('인기 영화 리스트 호출 실패:', error);
+    throw error;
+  }
+};
+
+//개봉 예정 영화 리스트 Get
+export const fetchUpcomingMovieList = async () => {
+  try {
+    const response = await client.get('/movie/upcoming');
+    return response.data;
+  } catch (error) {
+    console.error('개봉 예정 영화 호출 실패:', error);
+    throw error;
+  }
+};
+
+//영화 검색 결과 Get
+export const fetchSearchMovieList = async (searchData: string) => {
+  try {
+    const response = await client.get('/movie/search', {
+      params: { title: searchData, genre: searchData },
+    });
+    return response.data;
+  } catch (error) {
+    console.error('검색 API 호출 실패: ', error);
+    throw error;
+  }
+};
--- src/pages/main/MainPage.tsx
+++ src/pages/main/MainPage.tsx
@@ -3,130 +3,44 @@ import VideoSlider from './templates/VideoSlider';
 import * as S from './MainPage.style';
 import BoxofficeSlider from './templates/BoxofficeSlider';
 import MovieSwiperSlide from './templates/MovieSwiperSlide';
-import temp from 'assets/cardtemp.jpg';
-import boxofficetemp from 'assets/temp.svg';
 import { useAuth } from 'hooks/useAuth';
-import Movie from 'models/movie.model';
-import axios from 'axios';
-
-const movieAPI = [
-  { apiUrl: 'getapi1', title: '개봉 예정 영화' }, // 개봉 예정 영화
-  { apiUrl: 'getapi2', title: '인생 영화' }, // 인생 영화
-  { apiUrl: 'getapi3', title: '사용자 맞춤 추천 영화' }, // 추천 영화 `${user ? user : '빈빈'}님을 위한 추천 영화
-  { apiUrl: 'getapi4', title: '유저들이 주목하는 실시간 인기 영화' }, // 인기 영화
-];
+import { Movie } from 'models/movie.model';
+import { fetchBoxOfficeMovieList, fetchPopularMovieList, fetchUpcomingMovieList } from 'api/movie/movieApi';
 
 function MainPage() {
   const { user } = useAuth();
-  const [swiperData, setSwiperData] = useState<Array<{ title: string; movies: Movie[] }>>([
-    {
-      title: '개봉 예정 영화',
-      movies: [
-        {
-          title: 'ㅇㅇ',
-          movieId: 0,
-          tagline: '',
-          plot: '',
-          backdropImg: '',
-          mainImg: boxofficetemp,
-          releaseDate: new Date(),
-          rating: 0,
-          language: '',
-          runtime: 0,
-          ageRating: '',
-          reviewCount: 0,
-          directorName: [],
-          actorName: [],
-          actorImg: [],
-          genre: [],
-        },
-      ],
-    },
-    {
-      title: '인생 영화',
-      movies: [
-        {
-          title: 'ㅇㅇ',
-          movieId: 0,
-          tagline: '',
-          plot: '',
-          backdropImg: '',
-          mainImg: boxofficetemp,
-          releaseDate: new Date(),
-          rating: 0,
-          language: '',
-          runtime: 0,
-          ageRating: '',
-          reviewCount: 0,
-          directorName: [],
-          actorName: [],
-          actorImg: [],
-          genre: [],
-        },
-      ],
-    },
-    {
-      title: '사용자 맞춤 추천 영화',
-      movies: [
-        {
-          title: 'ㅇㅇ',
-          movieId: 0,
-          tagline: '',
-          plot: '',
-          backdropImg: '',
-          mainImg: boxofficetemp,
-          releaseDate: new Date(),
-          rating: 0,
-          language: '',
-          runtime: 0,
-          ageRating: '',
-          reviewCount: 0,
-          directorName: [],
-          actorName: [],
-          actorImg: [],
-          genre: [],
-        },
-      ],
-    },
-    {
-      title: '유저들이 주목하는 실시간 인기 영화',
-      movies: [
-        {
-          title: 'ㅇㅇ',
-          movieId: 0,
-          tagline: '',
-          plot: '',
-          backdropImg: '',
-          mainImg: boxofficetemp,
-          releaseDate: new Date(),
-          rating: 0,
-          language: '',
-          runtime: 0,
-          ageRating: '',
-          reviewCount: 0,
-          directorName: [],
-          actorName: [],
-          actorImg: [],
-          genre: [],
-        },
-      ],
-    },
-  ]);
-
-  // useEffect(() => { MovieSwiperSlide 4개 주제 목록 받아오기
-  //   Promise.all(
-  //     movieAPI.map(async (api) => {
-  //       const response = await axios.get(api.apiUrl); // 각 API에서 영화 데이터를 받아옴
-  //       return { title: api.title, movies: response.data }; // title과 movies를 함께 반환
-  //     })
-  //   ).then((responses) => {
-  //     setSwiperData(responses); // title과 movies를 가진 배열로 상태 업데이트
-  //   });
-  // }, []);
+  const [swiperData, setSwiperData] = useState<Array<{ title: string; movies: Movie[] }>>([]);
+  const [boxOfficeMovies, setBoxOfficeMovies] = useState<Movie[]>([]); // 박스오피스 데이터 상태
 
   useEffect(() => {
-    //박스 오피스 순위 axios
-  }, []);
+    const fetchData = async () => {
+      try {
+        const results = await Promise.all([
+          fetchUpcomingMovieList(),
+          fetchUpcomingMovieList(),
+          fetchUpcomingMovieList(),
+          // fetchPopularMovieList(),
+          // fetchBoxOfficeMovieList(),
+        ]);
+
+        const transformedData = [
+          { title: '개봉 예정 영화', movies: results[0] },
+          { title: '인생 영화', movies: results[1] },
+          { title: `${user ? user.data['nickname'] : '사용자'} 님을 위한 추천 영화`, movies: results[2] },
+          { title: '유저들이 주목하는 실시간 인기 영화', movies: results[2] },
+        ];
+        setSwiperData(transformedData);
+        console.log(results[0]);
+
+        //박스오피스용
+        setBoxOfficeMovies(results[2]);
+      } catch (error) {
+        console.error('데이터를 불러오는 데 실패했습니다:', error);
+      }
+    };
+
+    fetchData();
+  }, [user]);
 
   return (
     <>
@@ -136,13 +50,12 @@ function MainPage() {
         </S.VideoSliderWrapper>
         <S.Container>
           <S.BoxofficeSliderWrapper>
-            <BoxofficeSlider title="박스 오피스 순위" movies={swiperData[0].movies} />
+            <BoxofficeSlider title="박스 오피스 순위" movies={boxOfficeMovies} />
           </S.BoxofficeSliderWrapper>
 
           {swiperData.map((item, index) => (
             <S.MovieSwiperWrapper key={index}>
               <MovieSwiperSlide title={item.title} movies={item.movies} />
-              {/*movieData={item.movieData} */}
             </S.MovieSwiperWrapper>
           ))}
         </S.Container>
