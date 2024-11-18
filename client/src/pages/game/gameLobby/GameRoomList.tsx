20f007f79b2321372b74ebc926106473dfd59a55
vinivin153 <vinivin153@naver.com>
2024-11-18 18:07:35 +0900

    feat: 게임방 참여 구현

--- client/src/pages/game/gameLobby/GameRoomList.tsx
+++ client/src/pages/game/gameLobby/GameRoomList.tsx
@@ -1,25 +1,31 @@
+import { useGameRoomListQuery } from 'hooks/useGame';
 import * as S from './GameRoomList.style';
 import gameconsole from 'assets/bazzi.webp';
+
 export const GameRoomList = () => {
-  return (
-    <S.GameRoomListWrapper>
-      {Array.from({ length: 6 }, (_, x: number) => (
-        <S.GameRoomButton key={x}>
-          <S.ButtonImageWrapper>
-            <S.ButtonImage src={gameconsole} />
-          </S.ButtonImageWrapper>
-          <S.ButtonContentWarpper>
-            <S.ButtonTitleWrapper>
-              <S.RoomNumber>{String(x + 1).padStart(3, '0')}</S.RoomNumber>
-              <S.RoomTitle>즐겁게 게임 한판해요</S.RoomTitle>
-            </S.ButtonTitleWrapper>
-            <S.RoomStatusWapper>
-              <S.RoomStatus $isPlaying={false}>대기중</S.RoomStatus>
-              <S.RoomHeadCount>3/8</S.RoomHeadCount>
-            </S.RoomStatusWapper>
-          </S.ButtonContentWarpper>
-        </S.GameRoomButton>
-      ))}
-    </S.GameRoomListWrapper>
-  );
+  const { data } = useGameRoomListQuery();
+
+  if (data) {
+    return (
+      <S.GameRoomListWrapper>
+        {data.content.map((roomInfo) => (
+          <S.GameRoomButton key={roomInfo.id}>
+            <S.ButtonImageWrapper>
+              <S.ButtonImage src={gameconsole} />
+            </S.ButtonImageWrapper>
+            <S.ButtonContentWarpper>
+              <S.ButtonTitleWrapper>
+                <S.RoomNumber>{String(roomInfo.id).padStart(3, '0')}</S.RoomNumber>
+                <S.RoomTitle>{roomInfo.title}</S.RoomTitle>
+              </S.ButtonTitleWrapper>
+              <S.RoomStatusWapper>
+                <S.RoomStatus $isPlaying={roomInfo.status}>대기중</S.RoomStatus>
+                <S.RoomHeadCount>3/8</S.RoomHeadCount>
+              </S.RoomStatusWapper>
+            </S.ButtonContentWarpper>
+          </S.GameRoomButton>
+        ))}
+      </S.GameRoomListWrapper>
+    );
+  }
 };
