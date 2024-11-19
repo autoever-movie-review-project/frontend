b9f34151d3bfccd94f307d4733ae8a5c67991229
vinivin153 <vinivin153@naver.com>
2024-11-19 15:04:12 +0900

    feat: 게임방 유저 정보 API 연동

--- client/src/pages/game/gameRoom/GameRoomUser.tsx
+++ client/src/pages/game/gameRoom/GameRoomUser.tsx
@@ -4,29 +4,36 @@ import crown from 'assets/crown.png';
 import { useEffect, useState } from 'react';
 
 interface IGameRoomUserProps {
+  nickName: string;
+  profile: string;
   roomManager: boolean;
   message: string;
   isReady: boolean;
 }
 
-export const GameRoomUser = ({ roomManager, message, isReady }: IGameRoomUserProps) => {
-  const [chatMessage, setChatMessage] = useState(message);
+export const GameRoomUser = ({ nickName, profile, roomManager, message, isReady }: IGameRoomUserProps) => {
+  const [chatMessage, setChatMessage] = useState('');
 
   useEffect(() => {
-    setTimeout(() => {
-      setChatMessage('');
-    }, 3000);
-  }, []);
+    if (message) {
+      setChatMessage(message);
+      const timer = setTimeout(() => {
+        setChatMessage('');
+      }, 3000);
+
+      return () => clearTimeout(timer);
+    }
+  }, [message]);
 
   return (
     <S.GameRoomUserWrapper>
       {chatMessage ? <S.ChatDisplayBox>{chatMessage}</S.ChatDisplayBox> : <S.Blank />}
       <S.UserInfoBox>
-        {roomManager ? <img src={crown} /> : null}
+        {roomManager ? <img src={crown} alt="room manager" /> : null}
         <S.UserImageWrapper>
-          <S.UserImage src={bazzi} />
+          <S.UserImage src={profile || bazzi} alt="user" />
         </S.UserImageWrapper>
-        <S.UserName>김영화</S.UserName>
+        <S.UserName>{nickName}</S.UserName>
         <S.Score>{String(100).padStart(4, '0')}점</S.Score>
       </S.UserInfoBox>
       <S.Ready>{isReady ? 'Ready' : null}</S.Ready>
