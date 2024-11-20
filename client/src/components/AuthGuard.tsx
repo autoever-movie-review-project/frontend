ff2c533081e0274edc9e992e2ce01baa46d09a22
friend5hip <jwy97@naver.com>
2024-11-20 11:51:55 +0900

    design: 이모지 추가

--- client/src/components/AuthGuard.tsx
+++ client/src/components/AuthGuard.tsx
@@ -16,8 +16,8 @@ export const AuthGuard = ({ children, requireAuth = true }: AuthGuardProps) => {
     if (!isUserLoading) {
       if (requireAuth && !user) {
         // 인증이 필요한데 로그인이 안된 경우
-        toast('로그인 후 이용해주세요.', {
-          limit: 1, // 동시에 표시되는 토스트 메시지를 1개로 제한
+        toast('🍿 로그인 후 이용해주세요.', {
+          // limit: 1, // 동시에 표시되는 토스트 메시지를 1개로 제한
           toastId: 'auth-required', // 동일한 ID를 가진 토스트는 중복 표시되지 않음
         });
         navigate('/login');
